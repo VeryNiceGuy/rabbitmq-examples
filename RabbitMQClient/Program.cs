fc397562868fd2c95143f0805214fb9d5701a097
VeryNiceGuy <killingmesoftlywithchainsaw@gmail.com>
2022-01-23 16:04:40 +0500

    Uploading the files.

---
+++ RabbitMQClient/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQClient
+{
+    public abstract class BaseMessage
+    {
+        public string Guid { get; private set; }
+        public BaseMessage(string guid)
+        {
+            Guid = guid;
+        }
+    }
+
+    public class NodeScanRequestMessage : BaseMessage
+    {
+        public string Header { get; set; }
+        public string ScannerSerialNumber { get; private set; }
+        public string ComputerName { get; private set; }
+        public bool ScanEthalon { get; set; }
+        public NodeScanRequestMessage(string guid, string scannerSerialNumber, string computerName, string header, bool scanEthalon) : base(guid)
+        {
+            ScannerSerialNumber = scannerSerialNumber;
+            ComputerName = computerName;
+            Header = header;
+            ScanEthalon = scanEthalon;
+        }
+    }
+
+    public class NodeScanResponseMessage : BaseMessage
+    {
+        public string Ethalon { get; set; }
+        public string Identity { get; set; }
+        public string Message { get; set; }
+        public bool OverwriteMessage { get; set; }
+        public NodeScanResponseMessage(string guid, string identity, string ethalon) : base(guid)
+        {
+            Identity = identity;
+            Ethalon = ethalon;
+        }
+        public NodeScanResponseMessage(string guid, string message) : base(guid)
+        {
+            Message = message;
+        }
+    }
+
+    public class ConnectedClient { }
+
+    public interface IConnectionsService
+    {
+        Task<NodeScanResponseMessage> ScanOnLocalClientAsync(ConnectedClient client, NodeScanRequestMessage scanRequest, CancellationToken cancellationToken);
+    }
+
+    public class ConnectionsService : IConnectionsService
+    {
+        public async Task<NodeScanResponseMessage> ScanOnLocalClientAsync(ConnectedClient client, NodeScanRequestMessage scanRequest, CancellationToken cancellationToken)
+        {
+            return new NodeScanResponseMessage("2", "2", "2");
+        }
+    }
+
+    public enum MessageType
+    {
+        QueueRegistrationRequest,
+        QueueRegistrationResponse,
+        ScanRequest,
+        ScanResponse
+    }
+
+    public class Message
+    {
+        public MessageType MessageType { get; set; }
+        public string QueueName { get; set; }
+        public NodeScanRequestMessage ScanRequest { get; set; }
+        public NodeScanResponseMessage ScanResponse { get; set; }
+    } 
+
+    public class Client
+    {
+        private readonly IConnectionsService _connectionService;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly string _queueName;
+        private readonly EventingBasicConsumer _consumer;
+        private readonly BlockingCollection<Message> _incomingMessageQueue = new BlockingCollection<Message>();
+        private readonly IBasicProperties _properties;
+
+        public Client(IConnectionsService connectionService)
+        {
+            _connectionService = connectionService;
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _queueName = _channel.QueueDeclare().QueueName;
+            _consumer = new EventingBasicConsumer(_channel);
+            _properties = _channel.CreateBasicProperties();
+            _properties.ReplyTo = _queueName;
+            _consumer.Received += (model, eventArgs) =>
+            {
+                var text = Encoding.UTF8.GetString(eventArgs.Body);
+                var message = JsonConvert.DeserializeObject<Message>(text);
+
+                if(message.MessageType == MessageType.QueueRegistrationResponse)
+                    _incomingMessageQueue.Add(message);
+                else
+                {
+                    var scanResponse = _connectionService.ScanOnLocalClientAsync(new ConnectedClient(), message.ScanRequest, CancellationToken.None).Result;
+                    _channel.BasicPublish("", "rpc_queue", _properties,
+                        Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new Message{ MessageType = MessageType.ScanResponse, QueueName = message.QueueName, ScanResponse = scanResponse})));
+                }
+            };
+
+            _channel.BasicPublish("", "rpc_queue", _properties, Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(
+                new Message{ MessageType = MessageType.QueueRegistrationRequest })));
+            _channel.BasicConsume(consumer: _consumer, queue: _queueName, autoAck: true);
+
+            /* Diagnostic purposes only. */
+            var response = _incomingMessageQueue.Take();
+        }
+
+        public NodeScanResponseMessage SendScanRequest(NodeScanRequestMessage scanRequest)
+        {
+            _channel.BasicPublish("", "rpc_queue", _properties, Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(
+                new Message { MessageType = MessageType.ScanRequest,QueueName = _queueName, ScanRequest = scanRequest })));
+            return _incomingMessageQueue.Take().ScanResponse;
+        }
+
+        public void Close()
+        {
+            _connection.Close();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rpcClient = new Client(new ConnectionsService());
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+                rpcClient.SendScanRequest(new NodeScanRequestMessage("1", "1", "1", "1", true));
+
+
+            Console.ReadKey();
+            rpcClient.Close();
+        }
+    }
+}
---
+++ RabbitMQServer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQServer
+{
+    class Program
+    {
+        static List<string> _queueNames = new List<string>();
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "rpc_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.BasicQos(0, 1, false);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, eventArgs) =>
+                {
+                    var body = eventArgs.Body;
+                    var requestProperties = eventArgs.BasicProperties;
+                    var responseProperties = channel.CreateBasicProperties();
+
+                    var message = JObject.Parse(Encoding.UTF8.GetString(body));
+                    var messageType = message.GetValue("MessageType").ToObject<int>();
+
+                    switch (messageType)
+                    {
+                        case 0:
+                        {
+                            channel.BasicPublish("", requestProperties.ReplyTo, responseProperties,
+                                Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new Dictionary<string, string> { { "MessageType", "1" } })));
+                            _queueNames.Add(requestProperties.ReplyTo);
+                            channel.BasicAck(eventArgs.DeliveryTag, false);
+                            break;
+                        }
+                        case 2:
+                        {
+                            foreach (var name in _queueNames.Where(n => n != requestProperties.ReplyTo))
+                                channel.BasicPublish("", name, responseProperties, body);
+                            channel.BasicAck(eventArgs.DeliveryTag, false);
+                            break;
+                        }
+                        case 3:
+                        {
+                            channel.BasicPublish("", message.GetValue("QueueName").ToString(), responseProperties, body);
+                            channel.BasicAck(eventArgs.DeliveryTag, false);
+                            break;
+                        }
+                    }
+                };
+
+                channel.BasicConsume("rpc_queue", false, consumer);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
